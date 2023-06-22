39058a795460dbc5ebbd1bc033f229d2606acf79
lb <lbdeyo@gmail.com>
2023-06-22 14:17:18 -0500

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,14 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import {StaticImage} from "gatsby-plugin-image";
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby tutorial.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="../images/L.B._the_birds_and_the_bees_diagram_editorial_illustration_44214871-c101-4ed0-91e3-edae86d9ca9f.png"
+      />
     </Layout>
   );
 };
